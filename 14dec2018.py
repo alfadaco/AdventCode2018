558a7dc0ceb8561180b9a9ab41b5d52ca51c14f6
Ale <alessandro.falsone@gmail.com>
2018-12-28 16:01:37 +0100

    matches the first three examples but not the fourth one

---
+++ 14dec2018.py
@@ -0,0 +1,56 @@
+import data_14dec2018
+from math import floor
+
+data = data_14dec2018.data
+
+
+# Given an integer returns a list containing its digits in the same order
+def int2digits(n):
+    digits = []
+    while n > 0:
+        digits.insert(0, n % 10)
+        n = floor(n / 10)
+
+    return digits
+
+
+# Given the current recipes returns the updated scores and the new recipes
+def combine_recipes(scores, current_recipes):
+    # Compute score associated with the current recipes
+    sum_recipes_scores = sum([scores[recipe] for recipe in current_recipes])
+
+    # Add newly created recipes
+    new_scores = scores + int2digits(sum_recipes_scores)
+
+    # Compute new elves recipes
+    new_current_recipes = []
+    for recipe in current_recipes:
+        new_recipe = (recipe + scores[recipe] + 1) % len(new_scores)
+        new_current_recipes.append(new_recipe)
+
+    return new_scores, new_current_recipes
+
+
+# Number of elves
+n_elves = 2
+
+# Initial recipe for each elf
+current_recipes = list(range(0, n_elves))
+
+# Initial scores from data
+scores = int2digits(37)
+
+# Output info
+start_point = 2018
+length = 10
+end_point = start_point + length
+
+# Run game for n_turns
+while len(scores) < end_point:
+    #print(current_recipes, [scores[recipe] for recipe in current_recipes])
+
+    # Combine current recipes
+    scores, current_recipes = combine_recipes(scores, current_recipes)
+
+
+print('Scores = ' + ''.join([str(d) for d in scores[start_point:end_point]]))
