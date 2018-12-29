3890e3ecbc0097d5363efc484c43a841373c8761
cemorph <test@test.com>
2018-12-29 12:56:40 +0100

    added variable attak power solved second star

--- 15dec2018.py
+++ 15dec2018.py
@@ -9,11 +9,11 @@ class CreatureType(Enum):
 
 
 class Creature:
-    def __init__(self, type):
+    def __init__(self, type, attak):
         self.type = type
         self.next_turn = 0
         self.last_moved = -1
-        self.attack_power = 3
+        self.attack_power = attak
         self.hit_points = 200
 
     def moved(self, tick):
@@ -47,7 +47,7 @@ def print_position(map, creatures):
     print('')
 
 
-def init_maps(map_literal):
+def init_maps(map_literal, elves_power_attak, goblin_power_attak):
     size = (len(map_literal), len(max(map_literal, key=len)))
     map = np.ndarray(size, dtype='U1')
     creatures = np.ndarray(size, dtype=object)
@@ -60,10 +60,10 @@ def init_maps(map_literal):
             current_position = (i_r, i_c)
             if c == 'E':
                 map[current_position] = '.'
-                creatures[current_position] = Creature(CreatureType.Elf)
+                creatures[current_position] = Creature(CreatureType.Elf, elves_power_attak)
             elif c == 'G':
                 map[current_position] = '.'
-                creatures[current_position] = Creature(CreatureType.Goblin)
+                creatures[current_position] = Creature(CreatureType.Goblin, goblin_power_attak)
             else:
                 map[current_position] = c
 
@@ -146,132 +146,151 @@ def get_adiacent_enemies(map, creatures, current_position, current_type):
 
     return enemies
 
+def run_turn(map, creatures, current_round, debug):
+    death_creatures = []
+    for i_r in range(0, map.shape[0]):
+        for i_c in range(0, map.shape[1]):
+            current_position = (i_r, i_c)
+            if creatures[current_position] is None:
+                continue
+            if creatures[current_position].last_moved >= current_round:
+                continue
+
+            creatures[current_position].moved(current_round)
+
+            # Check if it nearby has a different creature type
+            neighbour_position = get_adiacent_enemies(map, creatures, current_position,
+                                                      creatures[current_position].type)
+
+            # If it has no neighbours, move
+            if len(neighbour_position) == 0:
+                in_range_map = select_in_range(map, creatures, creatures[current_position].type)
+                distances = select_distances(map, creatures, current_position)
+                reachable_point = np.where((in_range_map == '?') * (distances > 0))
+                if len(reachable_point[0]) == 0:
+                    continue
+
+                # Choose direction where to move
+                min_distance = min(distances[reachable_point])
+                chosen_point = np.asarray(np.where((in_range_map == '?') * (distances == min_distance)))
+                chosen_point = chosen_point[:, chosen_point[0] == min(chosen_point[0])]
+                chosen_point = chosen_point[:, chosen_point[1] == min(chosen_point[1])]
+                chosen_point = (chosen_point[0][0], chosen_point[1][0])
+
+                # Move a step in that direction
+                direction_chosen = False
+                # Try to move Up
+                if (not direction_chosen) and current_position[0] > 0:
+                    new_position = (current_position[0] - 1, current_position[1])
+                    if distances[new_position] >= 0 and \
+                            eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
+                        direction_chosen = True
+                # Try to move L
+                if (not direction_chosen) and current_position[1] > 0:
+                    new_position = (current_position[0], current_position[1] - 1)
+                    if distances[new_position] >= 0 and \
+                            eval_distance(map, creatures, new_position,
+                                          chosen_point) == min_distance - 1:
+                        direction_chosen = True
+                # Try to move R
+                if (not direction_chosen) and current_position[1] < map_size[1] - 1:
+                    new_position = (current_position[0], current_position[1] + 1)
+                    if distances[new_position] >= 0 and \
+                            eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
+                        direction_chosen = True
+                # Try to move Down
+                if (not direction_chosen) and current_position[0] < map_size[0] - 1:
+                    new_position = (current_position[0] + 1, current_position[1])
+                    if distances[new_position] >= 0 and \
+                            eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
+                        direction_chosen = True
+
+                if not direction_chosen:
+                    print('Error: no direction chosen')
+                    exit()
+
+                if creatures[new_position] is None:
+                    creatures[new_position] = creatures[current_position]
+                    creatures[current_position] = None
+
+                    current_position = new_position
+                else:
+                    print('Error!!!')
+                    exit()
+
+            # If after moving, it reached a enemy, attack
+            neighbour_position = get_adiacent_enemies(map, creatures, current_position, creatures[current_position].type)
+            if len(neighbour_position) > 0:
+                # get weakest neighbour
+                hit_neigh = [creatures[p].hit_points for p in neighbour_position]
+                weak_neigh = neighbour_position[hit_neigh.index(min(hit_neigh))]
+                if debug:
+                    print(creatures[current_position].type.value + ' in ' + str(current_position) + ' attack ' +
+                          creatures[weak_neigh].type.value + ' in ' + str(weak_neigh))
+                creatures[weak_neigh].get_hit(creatures[current_position].attack_power)
+                if debug:
+                    print(creatures[weak_neigh].type.value + ' in ' + str(weak_neigh) + ' is at ' +
+                          str(creatures[weak_neigh].hit_points) + 'HP')
+
+                if not creatures[weak_neigh].is_alive():
+                    enemy_type = creatures[weak_neigh].type
+                    death_creatures.append(creatures[weak_neigh])
+                    creatures[weak_neigh] = None
+
+                    # check if the game is won from a team
+                    if sum([c.type == enemy_type for c in creatures[creatures != None]]) == 0:
+                        return True, death_creatures
+
+    return False, death_creatures
+
+
 
 if __name__ == '__main__':
     debug = False
+    first_star = False
 
     #tests = data_15dec2018.map_tests
     tests = [[data_15dec2018.map_data, -1]]
     for test in tests:
         expected_score = test[1]
-        map, creatures, map_size = init_maps(test[0])
+        elves_attak = 3
+        goblin_attak = 3
+        map, creatures, map_size = init_maps(test[0], elves_attak, goblin_attak)
 
-        game_won = False
+        battle_won = False
         current_round = 0
 
-        while not game_won:
+        while not battle_won:
             current_round += 1
             if debug:
                 print('During round ' + str(current_round))
 
-            for i_r in range(0, map_size[0]):
-                if game_won: break
-                for i_c in range(0, map_size[1]):
-                    if game_won: break
-                    current_position = (i_r, i_c)
-                    if creatures[current_position] is None:
-                        continue
-                    if creatures[current_position].last_moved >= current_round:
-                        continue
+            battle_won, death_creatures = run_turn(map, creatures, current_round, debug)
 
-                    creatures[current_position].moved(current_round)
+            if not first_star:
+                death_elves = [c for c in death_creatures if c.type == CreatureType.Elf]
+                if len(death_elves) > 0:
+                    battle_won = False
+                    elves_attak += 1
+                    map, creatures, map_size = init_maps(test[0], elves_attak, goblin_attak)
+                    current_round = 0
 
-                    # Check if it nearby has a different creature type
-                    neighbour_position = get_adiacent_enemies(map, creatures, current_position, creatures[current_position].type)
-
-                    # If it has no neighbours, move
-                    if len(neighbour_position) == 0:
-                        in_range_map = select_in_range(map, creatures, creatures[current_position].type)
-                        distances = select_distances(map, creatures, current_position)
-                        reachable_point = np.where((in_range_map == '?')*(distances > 0))
-                        if len(reachable_point[0]) == 0:
-                            continue
-
-                        # Choose direction where to move
-                        min_distance = min(distances[reachable_point])
-                        chosen_point = np.asarray(np.where((in_range_map == '?') * (distances == min_distance)))
-                        chosen_point = chosen_point[:, chosen_point[0] == min(chosen_point[0])]
-                        chosen_point = chosen_point[:, chosen_point[1] == min(chosen_point[1])]
-                        chosen_point = (chosen_point[0][0], chosen_point[1][0])
-
-                        # Move a step in that direction
-                        direction_chosen = False
-                        # Try to move Up
-                        if (not direction_chosen) and current_position[0] > 0:
-                            new_position = (current_position[0] - 1, current_position[1])
-                            if distances[new_position] >= 0 and \
-                                    eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
-                                direction_chosen = True
-                        # Try to move L
-                        if (not direction_chosen) and current_position[1] > 0:
-                            new_position = (current_position[0], current_position[1] - 1)
-                            if distances[new_position] >= 0 and \
-                                    eval_distance(map, creatures, new_position,
-                                                  chosen_point) == min_distance - 1:
-                                direction_chosen = True
-                        # Try to move R
-                        if (not direction_chosen) and current_position[1] < map_size[1] - 1:
-                            new_position = (current_position[0], current_position[1] + 1)
-                            if distances[new_position] >= 0 and \
-                                    eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
-                                direction_chosen = True
-                        # Try to move Down
-                        if (not direction_chosen) and current_position[0] < map_size[0] - 1:
-                            new_position = (current_position[0] + 1, current_position[1])
-                            if distances[new_position] >= 0 and \
-                                    eval_distance(map, creatures, new_position, chosen_point) == min_distance - 1:
-                                direction_chosen = True
-
-                        if not direction_chosen:
-                            print('Error: no direction chosen')
-                            exit()
-
-                        if creatures[new_position] is None:
-                            creatures[new_position] = creatures[current_position]
-                            creatures[current_position] = None
-
-                            current_position = new_position
-                        else:
-                            print('Error!!!')
-                            exit()
-
-                    # If after moving, it reached a enemy, attack
-                    neighbour_position = get_adiacent_enemies(map, creatures, current_position, creatures[current_position].type)
-                    if len(neighbour_position) > 0:
-                        # get weakest neighbour
-                        hit_neigh = [creatures[p].hit_points for p in neighbour_position]
-                        weak_neigh = neighbour_position[hit_neigh.index(min(hit_neigh))]
-                        if debug:
-                            print(creatures[current_position].type.value + ' in ' + str(current_position) + ' attack ' +
-                                  creatures[weak_neigh].type.value + ' in ' + str(weak_neigh))
-                        creatures[weak_neigh].get_hit(creatures[current_position].attack_power)
-                        if debug:
-                            print(creatures[weak_neigh].type.value + ' in ' + str(weak_neigh) + ' is at ' +
-                                  str(creatures[weak_neigh].hit_points) + 'HP')
-
-                        if not creatures[weak_neigh].is_alive():
-                            enemy_type = creatures[weak_neigh].type
-                            creatures[weak_neigh] = None
-                            # check if the game is won from a team
-                            if sum([c.type == enemy_type for c in creatures[creatures != None]]) == 0:
-                                winner_type = creatures[current_position].type.value
-                                score = sum([c.hit_points for c in creatures[creatures != None]])
-                                last_complete_round = min([c.last_moved for c in creatures[creatures != None]])
-                                full_score = score*last_complete_round
-                                if debug:
-                                    print_position(map, creatures)
-                                print(winner_type + ' won with a score of ' + \
-                                      str(last_complete_round) + ' * ' + str(score) + ' = ' + str(full_score))
-                                game_won = True
-                                break
-
-            if not game_won:
+            if current_round > 0 and not battle_won:
                 last_complete_round = min([c.last_moved for c in creatures[creatures != None]])
                 if debug or current_round % 100 == 0:
                     print('after round ' + str(current_round))
                     print_position(map, creatures)
 
-        if expected_score > -1:
+        winner_type = creatures[creatures != None][0].type.value
+        score = sum([c.hit_points for c in creatures[creatures != None]])
+        last_complete_round = min([c.last_moved for c in creatures[creatures != None]])
+        full_score = score * last_complete_round
+        if debug:
+            print_position(map, creatures)
+        print('Elves attak power = ' + str(elves_attak))
+        print(winner_type + ' won with a score of ' + str(last_complete_round) + ' * ' + str(score) + ' = ' + str(full_score))
+
+        if expected_score > -1 and first_star:
             if expected_score == full_score:
                 print('test ' + str(tests.index(test)) + ' passed! ')
             else:
