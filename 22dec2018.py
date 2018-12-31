883da84feb9f7d8402571c33b1dcb68d50c07a40
Ale <alessandro.falsone@gmail.com>
2018-12-31 12:44:43 +0100

    code for dec 22 parts 1 and 2

--- 22dec2018.py
+++ 22dec2018.py
@@ -3,9 +3,9 @@ from data_22dec2018 import depth, target
 
 
 # Initialize the erosion level of the cave system
-buffer = 10
+buffer = 15
 dims = tuple([i+buffer+1 for i in target])
-el = np.NaN * np.ndarray(dims)
+el = np.zeros(dims, dtype=np.uint)
 
 # Set the erosion level of the mouth and the borders
 el[0, 0] = (0 + depth) % 20183
@@ -25,7 +25,7 @@ for d in range(1, max(dims)):
 el[target] = 0
 
 # Compute risk level
-rl = el % 3
+rl = (el % 3).astype(np.uint8)
 
 
 ##############
@@ -34,7 +34,8 @@ rl = el % 3
 
 # Compute risk level of the area
 rl_area = sum(sum(rl[0:target[0]+1, 0:target[1]+1]))
-print('\nThe risk level for the area is: ' + str(rl_area) + '\n\n')
+print('\nThe risk level for the area is: ' + str(rl_area) + '\n')
+
 
 ##############
 ### Part 2 ###
@@ -44,30 +45,103 @@ print('\nThe risk level for the area is: ' + str(rl_area) + '\n\n')
 # Region ids:    'rocky': 0      'wet': 1        'narrow': 2
 # Tool ids:      'neither': 0    'torch': 1      'gear': 2
 
+# Given a node compute its successors and the cost of reaching them
+def get_succ_cost(node):
+    # Initialize
+    succ = set()
+    cost = dict()
+
+    # Get node coordinates
+    (X, Y, T) = node
+
+    # Change tool
+    T_succ = int(3 - (rl[X, Y] + T))
+    node_succ = (X, Y, T_succ)
+    succ.add(node_succ)
+    cost[node_succ] = 7
+
+    # Move along X
+    if X > 0 and rl[X-1, Y] != T:
+        node_succ = (X-1, Y, T)
+        succ.add(node_succ)
+        cost[node_succ] = 1
+    if X < dims[0]-1 and rl[X+1, Y] != T:
+        node_succ = (X+1, Y, T)
+        succ.add(node_succ)
+        cost[node_succ] = 1
+
+    # Move along Y
+    if Y > 0 and rl[X, Y-1] != T:
+        node_succ = (X, Y-1, T)
+        succ.add(node_succ)
+        cost[node_succ] = 1
+    if Y < dims[1]-1 and rl[X, Y+1] != T:
+        node_succ = (X, Y+1, T)
+        succ.add(node_succ)
+        cost[node_succ] = 1
+
+    # Return
+    return (succ, cost)
+
+
+# Compute the minimum spanning tree using the mouth as the root
+def find_mst(init_state):
+    # Initialize predecessor map and states to check
+    P = dict()
+    P[init_state] = init_state
+    Q = {init_state}
+
+    # Initialize cost map
+    C = dict()
+    C[init_state] = 0
+
+    # Run Dijikstra algorithm for finding the shortest path
+    while len(Q) > 0:
+        state = Q.pop()
+        (succ_state, succ_cost) = get_succ_cost(state)
+        for ss in succ_state:
+            if (ss not in P.keys()) or (C[state] + succ_cost[ss] < C[ss]):
+                P[ss] = state
+                C[ss] = C[state] + succ_cost[ss]
+                Q.add(ss)
+
+    # Return predecessor list and associated cost
+    return (P, C)
+
+
 # Define extended initial and target state
-state_init = (0, 0, 1)
-state_target = target + (1,)
+initial_state = (0, 0, 1)
+target_state = target + (1,)
 
-# Create map with current equipped tool
-state = np.reshape(rl, rl.shape + (1,))
-state = np.tile(state, (1, 1, 3))
+# Compute the mst
+(P_state, C_state) = find_mst(initial_state)
 
-# Delete incompatible states
-for t in range(0, 3):
-    view = state[:, :, t]
-    view[view == t] = np.NaN
-
-# Clear matrix
-state[np.logical_not(np.isnan(state))] = True
-state[np.isnan(state)] = False
+# Print time needed for reaching the target
+print('Reaching the target requires: ' +
+      str(C_state[target_state]) + ' minutes\n\n')
 
 
 # Visual map
+print('Cave system map')
 image = np.ndarray(rl.shape, dtype='U8')
 image[rl == 0] = '.'    # rocky
 image[rl == 1] = '='    # wet
 image[rl == 2] = '|'    # narrow
 image[(0, 0)] = 'M'
 image[target] = 'T'
+for row in image:
+    print(''.join(row))
+
+# Compute route
+curr_state = target_state
+while True:
+    curr_state = P_state[curr_state]
+    if curr_state == initial_state:
+        break
+    else:
+        image[curr_state[0], curr_state[1]] = 'x'
+
+# Visual map of the route
+print('\nCave system map with optimal route')
 for row in image:
     print(''.join(row))
